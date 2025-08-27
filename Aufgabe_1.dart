6cd19c7eb5cdfcf6974598284f5710dcaac7eba1
Farhad <farhad.ahmadi@outlook.de>
2025-08-27 05:26:44 -0700

    init

---
+++ Aufgabe_1.dart
@@ -0,0 +1,11 @@
+// Die Funktion, die eine Zahl ausgibt
+void printNumber(int number) {
+  print("Die Zahl ist: $number");
+}
+
+// Die Hauptfunktion
+void main() {
+  printNumber(5);
+  printNumber(42);
+  printNumber(99);
+}
